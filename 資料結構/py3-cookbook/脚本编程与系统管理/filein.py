072efa9be030f7bb56f8312f6157243ae1569c77
ZhongIan <po39086m@yahoo.com.tw>
2019-10-15 23:59:14 +0800

    add

--- 資料結構/py3-cookbook/脚本编程与系统管理/filein.py
+++ 資料結構/py3-cookbook/脚本编程与系统管理/filein.py
@@ -1,6 +1,9 @@
 
 import fileinput
 import subprocess
+
+# ===== sys ======
+# python "...\filein.py" FileinputTest.txt
 import sys
 a=sys.argv[0]
 b=sys.argv[1]
@@ -8,12 +11,17 @@ b=sys.argv[1]
 print("filename:",a)
 print("param1:",b)
 
-
-file = input("請輸入...")
-print(file)
-
 with fileinput.input(b) as f_input:
     for line in f_input:
         print(line, end='')
+
+# ===== input =====
+#file = input("請輸入...")
+#print(file)
+#
+#with fileinput.input(file) as f_input:
+#    for line in f_input:
+#        print(line, end='')
         
+# 暫停        
 subprocess.call("pause",shell=True)
