e275552128967eba293fc2b81a79b51aefc77c8a
ZhongIan <po39086m@yahoo.com.tw>
2019-10-17 22:29:55 +0800

    add

---
+++ 資料結構/py3-cookbook/脚本编程与系统管理/somelib.py
@@ -0,0 +1,10 @@
+
+import logging
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+# Example function (for testing)
+def func():
+    print('--- in somelib.func() ---')
+    log.critical('A Critical Error!')
+    log.debug('A debug message')
