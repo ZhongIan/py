1b9d40970793ff98be9a7f06f90fd14dc8d573d1
ZhongIan <po39086m@yahoo.com.tw>
2019-10-21 23:57:48 +0800

    add

---
+++ 資料結構/學習_廖雪峰/錯誤、調式、測試/Student.py
@@ -0,0 +1,16 @@
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def get_grade(self):
+        if self.score > 100:
+            raise ValueError
+        elif self.score >= 80:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        elif self.score >= 0:
+            return 'C'
+        else:
+            raise ValueError
