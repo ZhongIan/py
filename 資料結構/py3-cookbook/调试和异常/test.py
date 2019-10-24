f79d9a2fe90a6809311993f2fefa20cb4fdb20f2
ZhongIan <po39086m@yahoo.com.tw>
2019-10-24 23:35:52 +0800

    add

---
+++ 資料結構/py3-cookbook/调试和异常/test.py
@@ -0,0 +1,14 @@
+import errno
+import unittest
+
+class TestIO(unittest.TestCase):
+    def test_file_not_found(self):
+        try:
+            f = open('/file/not/found')
+        except IOError as e:
+            self.assertEqual(e.errno, errno.ENOENT)
+
+        else:
+            self.fail('IOError not raised')
+if __name__ == '__main__':
+    unittest.main()
