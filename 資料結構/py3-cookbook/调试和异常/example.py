24416a0912dd57bf5d33c4638e9825756464230e
ZhongIan <po39086m@yahoo.com.tw>
2019-10-26 11:59:02 +0800

    add

---
+++ 資料結構/py3-cookbook/调试和异常/example.py
@@ -0,0 +1,8 @@
+import warnings
+
+def func(x, y, logfile=None, debug=False):
+    if logfile is not None:
+         warnings.warn('logfile argument deprecated', DeprecationWarning)
+            
+if __name__=='__main__':
+    func(1,2 )
